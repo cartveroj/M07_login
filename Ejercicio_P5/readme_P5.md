aa37468ac99f3da85d986460e2d730f2d7a49c1c
cartveroj <113618938+cartveroj@users.noreply.github.com>
2023-10-28 14:27:27 +0200

    Revert "ejercicio5_sinConflictos"

--- Ejercicio_P5/readme_P5.md
+++
@@ -1,3 +0,0 @@
-## PRÀCTICA 5 - LOGIN AMB COOKIES
-* Veronica Cartagena 
-[link_video_Ejercicio_P5](https://drive.google.com/file/d/15FJKuY-R_o_T1DJuoVua1fpX-GYGbVRK/view?usp=drive_link)
--- Ejercicio_P5/veronica_P5/cookies/deleteCookie.php
+++
@@ -1,5 +0,0 @@
-<?php
-//Archivo que se encarga de eliminar la cookie y redirige a la página index.php de Cookies
-setcookie("idioma_seleccionado","", time() -1);
-header("Location: index.php");
-?>
